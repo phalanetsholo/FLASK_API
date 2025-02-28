6b34fe6e24e7fd041a1310036dfde8445a4b47f2
Tshepiso28 <tshepisostudent@gmail.com>
2025-02-28 12:56:35 +0200

    indentation

--- app/models/employees.py
+++ app/models/employees.py
@@ -35,7 +35,7 @@ def update_employee(id, first_name, last_name, email, phone, department):
     cursor.close()
     conn.close()
     
-    def delete_employee(id):
+def delete_employee(id):
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute('DELETE FROM employees WHERE id = %s', (id,))
