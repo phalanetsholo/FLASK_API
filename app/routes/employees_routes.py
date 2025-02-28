9db70b1cdb86fc62c23705d693100264278ef374
Ntsako-Coder <janelntsako@gmail.com>
2025-02-28 13:44:39 +0200

    employees routes updated

---
+++ app/routes/employees_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.controllers.employees_controller import add_employee, view_employees, update_employee_route, delete_employee_route
+
+employee_bp = Blueprint('employee', __name__)
+
+employee_bp.route('/view', methods=['GET'])(view_employees)
+employee_bp.route('/add', methods=['POST'])(add_employee)
+employee_bp.route('/update/<int:id>', methods=['PUT'])(update_employee_route)
+employee_bp.route('/delete/<int:id>', methods=['DELETE'])(delete_employee_route)
